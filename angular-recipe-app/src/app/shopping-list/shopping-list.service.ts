f49f0ea22373244f06622dfe5ce242bbe0e2c832
lsimps204 <semioticghost1@gmail.com>
2018-07-10 14:23:28 +0100

    Template driven forms section finished

--- angular-recipe-app/src/app/shopping-list/shopping-list.service.ts
+++ angular-recipe-app/src/app/shopping-list/shopping-list.service.ts
@@ -5,6 +5,7 @@ import { Subject } from 'rxjs';
 export class ShoppingListService {
 
     ingredientsChanged = new Subject<Ingredient[]>()
+    startedEditing = new Subject<number>()
 
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
@@ -16,12 +17,42 @@ export class ShoppingListService {
     }
     
     addIngredient(ingredient: Ingredient) {
-        this.ingredients.push(ingredient)
-        this.ingredientsChanged.next(this.ingredients.slice())
+        if (this.validIngredient(ingredient)) {
+            this.ingredients.push(ingredient)
+            this.ingredientsChanged.next(this.ingredients.slice())
+        }
     }
 
     addIngredients(ingredients: Ingredient[]) {
         this.ingredients.push(...ingredients) // spread operator (...): spreads the array of ingredients into single ingredients
         this.ingredientsChanged.next(this.ingredients.slice())
+    }
+
+    getIngredient(index: number) {
+        return this.ingredients[index]
+    }
+
+    updateIngredient(index: number, newIngredient: Ingredient) {
+        if (this.validIngredient(newIngredient, index)) {
+            this.ingredients[index] = newIngredient
+            this.ingredientsChanged.next(this.ingredients.slice())
+        }
+    }
+
+    deleteIngredient(index: number) {
+        this.ingredients.splice(index, 1)
+        this.ingredientsChanged.next(this.ingredients.slice())
+    }
+
+    validIngredient(ingredient: Ingredient, omitIndex: number = -1) {
+        let found: number
+        if (omitIndex != -1) {
+            let filteredList = this.ingredients.filter((v,i) => i != omitIndex)
+            found = filteredList.findIndex(val => val.name === ingredient.name)
+        } else {
+            found = this.ingredients.findIndex(val => val.name === ingredient.name)
+        }
+        console.log(this.ingredients)
+        return found == -1
     }
 }
